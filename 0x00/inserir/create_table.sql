316c867c3a2dd3a904f346ac084f9dc6f40e241c
Pedro Schneider <wschneider@ciandt.com>
2022-07-27 10:51:11 -0300

    json

---
+++ 0x00/inserir/create_table.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE CLIENTE (
+    id       INTEGER PRIMARY KEY NOT NULL,
+    nome     VARCHAR(80) NOT NULL,
+    email    VARCHAR(40) NOT NULL,
+    telefone INT
+);
+
+INSERT INTO CLIENTE (id, nome, email, telefone)
+VALUES (1, 'Betty', 'betty@techcamps.com', 12345678);
+
+INSERT INTO CLIENTE (id, nome, email, telefone)
+VALUES (2, 'Betty', 'betty@techcamps.com', 12345678);
+
+INSERT INTO CLIENTE (id, nome, email, telefone)
+VALUES (3, 'Betty', 'betty@techcamps.com', 12345678);
+
+INSERT INTO CLIENTE (id, nome, email, telefone)
+VALUES (4, 'Betty', 'betty@techcamps.com', 12345678);
+
+INSERT INTO CLIENTE (id, nome, email, telefone)
+VALUES (5, 'Betty', 'betty@techcamps.com', 12345678);
