c6d17f79b4ad66627492354932b14fd210ff075e
Pedro Schneider <wschneider@ciandt.com>
2022-07-28 10:07:59 -0300

    add changes

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE CLIENTE (
+    id       INTEGER PRIMARY KEY NOT NULL,
+    nome     VARCHAR(80) NOT NULL,
+    email    VARCHAR(40) NOT NULL,
+    telefone INT
+);
+
+CREATE TABLE sqlite_sequence(name, seq);
